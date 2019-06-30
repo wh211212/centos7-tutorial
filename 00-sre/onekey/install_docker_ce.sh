059684e84a87e13c007f4b07892d7005f7c0fd05
shaonbean <shaonbean@qq.com>
2019-06-30 21:17:59 +0800

    add install docker scripts

---
+++ 00-sre/onekey/install_docker_ce.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# command from office website （https://docs.docker.com/）
+
+# remove old docker
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+# config docker_ce repo
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# install latest docker ce
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker && sudo systemctl enable docker
+
+# use office shell
+
+#curl -fsSL https://get.docker.com -o get-docker.
+#sudo sh get-docker.sh
